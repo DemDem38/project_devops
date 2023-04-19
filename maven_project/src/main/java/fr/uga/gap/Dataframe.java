8d47613bf3b197bd2332b1145950b9dddd66b416
Pierre-Mar <pierre.marjolin@gmail.com>
2023-04-19 09:05:12 +0200

    Modification creation Dataframe

--- maven_project/src/main/java/fr/uga/gap/Dataframe.java
+++ maven_project/src/main/java/fr/uga/gap/Dataframe.java
@@ -1,19 +1,26 @@
 package fr.uga.gap;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Dataframe {
+public class Dataframe<L> {
     
-    ArrayList<String> nameLines;
-    ArrayList<Series> columns;
+    HashMap<L, Series<Object>> mapSeries;
+    ArrayList<Integer> index;
 
-    public Dataframe(String[] nl, String[] nc, int[][] t) {
-        nameLines = new ArrayList<String>();
-        columns = new ArrayList<Series>();
+    public Dataframe(L[] ind, Object[][] o) {
+        mapSeries = new HashMap<>();
+        index = new ArrayList<>();
 
-        for (String elem : nl) {
-            nameLines.add((elem));
+        for (int i = 0; i < o.length; i++) {
+            Series<Object> s = new Series<>(o[i]);
+            mapSeries.put(ind[i], s);
+            index.add(i);
         }
+
+        // for (String elem : nl) {
+        //     nameLines.add((elem));
+        // }
 
         // for (int i = 0; i < nc.length; i++) {
         //     columns.add(new Series(t[i], nc[i]));
--- maven_project/src/test/java/fr/uga/gap/DataframeTest.java
+++ maven_project/src/test/java/fr/uga/gap/DataframeTest.java
@@ -4,16 +4,26 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-
 public class DataframeTest extends TestCase{
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
+
+
+    Dataframe<String> dataframe;
+    String[] name;
+    Object[][] object;
+
     public DataframeTest( String testName )
     {
         super( testName );
+        name = new String[]{"A", "B", "C"};
+        object = new Object[][]{{0, 1, 3},
+                                            {"a", "b", "c"},
+                                            {0.0, 1.0, 2.0}};
+        dataframe = new Dataframe<>(name, object);
     }
 
     /**
@@ -27,8 +37,20 @@ public class DataframeTest extends TestCase{
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testDataframeCreation()
+    {   
+        int i = 0;
+        int j;
+        for (String n: dataframe.mapSeries.keySet()) {
+            assertEquals(n, name[i]);
+            Series<Object> series = dataframe.mapSeries.get(n);
+            j = 0;
+            for (Object o: series.list) {
+                assertEquals(o, object[i][j]);
+                j++;
+            }
+            i++;
+
+        }
     }
 }
