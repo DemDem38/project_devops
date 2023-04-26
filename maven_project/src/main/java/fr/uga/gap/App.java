cd0cfd1c7479ebaffe03a9bc82a5994e221b39da
perringe <perringerome.pro@gmail.com>
2023-04-26 09:54:23 +0200

    tests in main to see print

--- maven_project/src/main/java/fr/uga/gap/App.java
+++ maven_project/src/main/java/fr/uga/gap/App.java
@@ -9,14 +9,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Dataframe<String> dataframe;
+        Dataframe dataframe;
         String[] name;
         Object[][] object;
         name = new String[]{"Age", "Nationalité", "Sexe"};
         object = new Object[][]{{35, 63, 39},{"Français", "Belge", "Suisse"},{'H', 'F', 'H'}};
-        dataframe = new Dataframe<>(name, object);
+        dataframe = new Dataframe(name, object);
         System.out.println(dataframe);
-        System.out.println(dataframe.printFirstLines(2));
+        System.out.println(dataframe.printFirstLines(1));
         System.out.println(dataframe.printLastLines(2));
     }
 }
